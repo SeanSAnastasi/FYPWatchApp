43d377e23235c5297c16396b2f9dff7bf91108b8
Sean <sean.s.anastasi@gmail.com>
2021-03-07 21:20:02 +0100

    Final Watch app

--- fyp-watch-app/app/index.js
+++ fyp-watch-app/app/index.js
@@ -1,1 +1,90 @@
-console.log('Hello world!');
+import document from "document";
+import { HeartRateSensor } from "heart-rate";
+import * as fs from "fs";
+import {outbox} from "file-transfer";
+import { listDirSync } from "fs";
+
+const hrmLabel = document.getElementById("hrm-label");
+const hrmData = document.getElementById("hrm-data");
+const sendToDB = document.getElementById("send-to-db");
+const sensors = [];
+
+const hrmJSON = [];
+
+var dbOutput = false;
+var newReading = false;
+
+if (HeartRateSensor) {
+  const hrm = new HeartRateSensor({ frequency: 1 });
+  hrm.addEventListener("reading", () => {
+    
+    if(dbOutput){
+        hrmData.text = JSON.stringify({
+            heartRate: hrm.heartRate ? hrm.heartRate : 0
+          });
+        newReading = true;
+        var date= new Date().toString();
+
+        hrmJSON.push( {
+          date: date,
+          heartRate: hrm.heartRate
+        });
+          
+          
+    }else{
+        if(newReading){
+            hrmData.text = "{ ... }";
+            const jsonData = document.getElementById("json-data");
+            
+            fs.writeFileSync("heartrate.txt", hrmJSON, "json");
+            console.log(hrmJSON);
+            newReading = false;
+            let dirIter = "";
+            const listDir = listDirSync("/private/data");
+            while((dirIter = listDir.next()) && !dirIter.done) {
+              console.log(dirIter.value);
+            }
+        }
+    }
+  });
+  sensors.push(hrm);
+  hrm.start();
+} else {
+  hrmLabel.style.display = "none";
+  hrmData.style.display = "none";
+}
+
+let Rect = document.getElementById("button-1");
+
+Rect.addEventListener("click", (evt) => {
+  dbOutput = !dbOutput;
+  
+  
+
+  if(dbOutput){
+      Rect.text="READING";
+  }else{
+      Rect.text="READ";
+  }
+});
+
+let sendData = document.getElementById("button-2");
+sendData.addEventListener("click", (evt) => {
+
+  outbox
+  .enqueueFile("/private/data/heartrate.txt")
+  .then(ft => {
+    console.log(`Transfer of ${ft.name} successfully queued.`);
+  })
+  .catch(err => {
+    console.log(`Failed to schedule transfer: ${err}`);
+
+  })
+
+});
+
+
+// display.addEventListener("change", () => {
+//   // Automatically stop all sensors when the screen is off to conserve battery
+//   display.on ? sensors.map(sensor => sensor.start()) : sensors.map(sensor => sensor.stop());
+// });
--- fyp-watch-app/companion/index.js
+++ fyp-watch-app/companion/index.js
@@ -1,1 +1,33 @@
-console.log('Hello world!');
+import { inbox } from "file-transfer";
+
+async function processAllFiles() {
+  let file;
+  while ((file = await inbox.pop())) {
+    var payload = await file.text();
+   
+    console.log(`file contents: ${payload}`);
+    
+    
+    
+    const url = "https://fyp-sensors.web.app/data";
+    var params = {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer',
+        body: payload
+        }
+
+    const res = await fetch(url, params);
+    console.log(res);
+
+  }
+}
+
+inbox.addEventListener("newfile", processAllFiles);
+
+processAllFiles()
